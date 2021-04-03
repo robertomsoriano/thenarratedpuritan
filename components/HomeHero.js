c2415309302262866bedd533d22ec4f8c1ba3232
Roberto <robertmsoriano@gmail.com>
2021-04-03 12:03:03 -0500

    .

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -4,16 +4,20 @@ import Link from 'next/link'
 import { Image, Button } from 'react-bootstrap'
 const HomeHero = () => {
     return (
-        <div className={`d-flex align-items-center justify-content-center flex-column mt-5`} >
-            <div className={`row`}>
-                <div className={`col-12`}><h1>The Narrated Puritan</h1></div></div>
+        <div className={`d-flex align-items-center justify-content-center flex-row`} style={{ minHeight: '70vh' }}>
+            {/* <div className={`row`}>
+                <div className={`col-12`}><h1>The Narrated Puritan</h1></div></div> */}
 
-            <div className={`row flex-column align-items-center justify-content-center`} style={{ background: 'rgb(244,247,250)', width: '100%' }}>
-                <div className={`text-center my-5`}>
+            <div className={`row  align-items-center justify-content-center`} style={{ background: 'rgb(244,247,250)', width: '100%' }}>
+                <div className={`col-12 d-flex flex-column align-items-center justify-content-center my-5`}>
+                    <h1>The Narrated Puritan</h1>
+                    <h2>Old Treasures Tith Abiding Relevance</h2>
+                </div>
+                <div className={`text-center mb-5`}>
                     {/* <Image src="/profile.jpeg" alt="me" width="200" height="200" rounded /> */}
                     <Image src="/profile.jpeg" rounded style={{ width: '300px', height: '300px' }} className={`shadow-sm`} />
                 </div>
-                <div className={`col-sm-6 col-lg-6 mb-5`} style={{ minHeight: '50vh', }}>
+                <div className={`col-sm-6 col-lg-6 mb-5`}>
                     <div style={{ width: '100%' }}><p >
                         Though these narrations now span the last 35 years and consist of the largest collection of unique names of pastors in reformed and Puritan churches since the reformation in audiobook format, they mostly are the best of the last 6 years of a collection of the most solemn and searching, consoling and counseling, casuistical and Christ-centered sermons since the Reformation.
                         </p></div>
